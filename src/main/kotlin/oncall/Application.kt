0db4688a9ddade3ee70c5d5bfa3d549bbc49517b
이용인 <yongin0517@gmail.com>
2024-11-27 02:25:30 +0900

    feat: 입력 요구 사항 구현

--- src/main/kotlin/oncall/Application.kt
+++ src/main/kotlin/oncall/Application.kt
@@ -1,5 +1,14 @@
 package oncall
 
+import oncall.controller.Controller
+import oncall.controller.InputController
+import oncall.domain.CalendarGenerator
+import oncall.validator.InputValidator
+
 fun main() {
-    TODO("프로그램 구현")
+    val inputValidator = InputValidator()
+    val inputController = InputController(inputValidator)
+    val calendarGenerator = CalendarGenerator()
+    val controller = Controller(inputController, calendarGenerator)
+    controller.run()
 }
