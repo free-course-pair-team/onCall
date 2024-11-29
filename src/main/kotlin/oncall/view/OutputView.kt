b196ed7d1cd4b9de66354fae04756d7bb4aabb7c
이용인 <yongin0517@gmail.com>
2024-11-29 13:56:01 +0900

    feat: 결과 출력 기능 구현

--- src/main/kotlin/oncall/view/OutputView.kt
+++ src/main/kotlin/oncall/view/OutputView.kt
@@ -1,4 +1,13 @@
 package oncall.view
 
+import oncall.model.OnCallWorkSchedule
+
 class OutputView {
+    fun printOnCallWorkSchedule(onCallWorkerSchedule: OnCallWorkSchedule) {
+        onCallWorkerSchedule.getResult().forEach {
+            //5월 5일 금(휴일) 루루
+            val holidayMark = if (it.isHoliday) "(휴일)" else ""
+            println("${it.month}월 ${it.date}일 ${it.dayOfWeek.dayName}${holidayMark} ${it.worker}")
+        }
+    }
 }
